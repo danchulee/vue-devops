132203fd21810afb4652bd73ff07f156525e2e93
danchulee <danchulee@naver.com>
2021-06-29 14:56:13 +0900

    EDIT vue (div tag)

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your 이주원 App" />
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
 </template>
 
 <script>
