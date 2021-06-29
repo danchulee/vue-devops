b44a3081dcf6d224c88debc0f570687814bacd08
danchulee <danchulee@naver.com>
2021-06-29 12:09:33 +0900

    EDIT

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,14 @@
 module.exports = {
-  extends: 'standard',
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ['plugin:vue/essential'],
   rules: {
-    indent: ['error', 4],
-    semi: ['error', 'always'],
-    'no-trailing-spaces': 0,
-    'keyword-spacing': 0,
-    'no-unused-vars': 1,
-    'no-multiple-empty-lines': 0,
-    'space-before-function-paren': 0,
-    'eol-last': 0,
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
   },
 };
