4e2a76b334613652805b2dbc4229e094f830c063
danchulee <danchulee@naver.com>
2021-06-29 14:51:37 +0900

    EDIT deploy.yml

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <HelloWorld msg="Welcome to Your 이주원 App" />
 </template>
 
 <script>
